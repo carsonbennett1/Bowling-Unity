1a97b62a85255d3b23fc50140a07273f33a533af
Carson Bennett <carben2004@gmail.com>
2025-02-10 20:46:49 -0800

    working-on-lateral-movement-incomplete

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private InputManager inputManager;
+    [SerializeField] private float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        // Adding MovePlayer as a listener to the onMove event
+        inputManager.OnMove.AddListener(MovePlayer);
+
+        rb = GetComponent<Rigidbody>();
+    }
+    
+    // Listen to left and right inputs from user
+    private void MovePlayer(Vector2 direction)
+    {
+        Vector3 moveDirection = new(direction.x, 0f, direction.y);
+        rb.AddForce(speed * moveDirection);
+    }
+}
